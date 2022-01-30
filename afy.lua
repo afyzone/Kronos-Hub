ff9c2b02642120a4bcd607bc5aa8f0c13bafb38a
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-30 15:48:31 -0800

    Update afy.lua

--- afy.lua
+++ afy.lua
@@ -1,5 +1,5 @@
 -- // Dependencies
-_G.PRED = 0.167
+_G.PRED = 1
 local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/GamePatches/2788229376.lua"))()
 Aiming.TeamCheck(false)
 Aiming.ShowFOV = false

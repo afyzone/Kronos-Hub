fe8c9625a4a19a204d61044089aabf40e7eba661
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-30 15:56:54 -0800

    Update afy.lua

--- afy.lua
+++ afy.lua
@@ -10,12 +10,10 @@ local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
-
 -- // Vars
 local LocalPlayer = Players.LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
 local CurrentCamera = Workspace.CurrentCamera
-
 local DaHoodSettings = {
     SilentAim = true,
     AimLock = false,
@@ -23,28 +21,23 @@ local DaHoodSettings = {
     AimLockKeybind = Enum.KeyCode.E
 }
 getgenv().DaHoodSettings = DaHoodSettings
-
 -- // Overwrite to account downed
 function Aiming.Check()
     -- // Check A
     if not (Aiming.Enabled == true and Aiming.Selected ~= LocalPlayer and Aiming.SelectedPart ~= nil) then
         return false
     end
-
     -- // Check if downed
     local Character = Aiming.Character(Aiming.Selected)
     local KOd = Character:WaitForChild("BodyEffects")["K.O"].Value
     local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
-
     -- // Check B
     if (KOd or Grabbed) then
         return false
     end
-
     -- //
     return true
 end
-
 -- // Hook
 local __index
 __index = hookmetamethod(game, "__index", function(t, k)
@@ -52,24 +45,19 @@ __index = hookmetamethod(game, "__index", function(t, k)
     if (t:IsA("Mouse") and (k == "Hit" or k == "Target") and Aiming.Check()) then
         -- // Vars
         local SelectedPart = Aiming.SelectedPart
-
         -- // Hit/Target
         if (DaHoodSettings.SilentAim and (k == "Hit" or k == "Target")) then
             -- // Hit to account prediction
             local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
-
             -- // Return modded val
             return (k == "Hit" and Hit or SelectedPart)
         end
     end
-
     -- // Return
     return __index(t, k)
 end)
-
 local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
-
 mouse.KeyDown:Connect(function(key)
     if key == "v" then
         if Aiming.Enabled == false then
@@ -79,19 +67,16 @@ mouse.KeyDown:Connect(function(key)
         end
     end
 end)
-
 mouse.KeyDown:Connect(function(key)
 	if key == "l" then
 		DaHoodSettings.Prediction = PingNumber / 1000 + _G.PRED * -1
 	end
 end)
-
 RunService.RenderStepped:Connect(function()
-
     local ping = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString()
     local Value = tostring(ping)
     local pingValue = Value:split(" ")
     local PingNumber = pingValue[1]
-    
     DaHoodSettings.Prediction = PingNumber / 1000 + _G.PRED
+    end
 end)

0b2626cb0cfe012144e96c609e6fa5bc9e19775c
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-30 16:28:09 -0800

    Update afy.lua

--- afy.lua
+++ afy.lua
@@ -1,5 +1,5 @@
 -- // Dependencies
-_G.PRED = 0.027
+_G.PRED = 0.057
 local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/GamePatches/2788229376.lua"))()
 Aiming.TeamCheck(false)
 Aiming.ShowFOV = false
@@ -23,7 +23,7 @@ local CurrentCamera = Workspace.CurrentCamera
 local DaHoodSettings = {
 	SilentAim = true,
 	AimLock = false,
-	Prediction = 0.125,
+	Prediction = 0.105,
 	AimLockKeybind = Enum.KeyCode.E
 }
 getgenv().DaHoodSettings = DaHoodSettings

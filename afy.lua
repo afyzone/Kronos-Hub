23cb234fe6e6b4f1b8eeef7c2244b9a026dbb4b4
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-30 19:47:25 -0800

    Update afy.lua

--- afy.lua
+++ afy.lua
@@ -23,7 +23,7 @@ local CurrentCamera = Workspace.CurrentCamera
 local DaHoodSettings = {
 	SilentAim = true,
 	AimLock = false,
-	Prediction = 0.105,
+	Prediction = 0.165,
 	AimLockKeybind = Enum.KeyCode.E
 }
 getgenv().DaHoodSettings = DaHoodSettings

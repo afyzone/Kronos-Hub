e36d7ac2c8c7ae9c1929b872d872dd3bd34bcf0e
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-30 17:59:03 -0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,5 +1,5 @@
-uid = game.Players.LocalPlayer.userId
-plist = {"Sunday", "1299452235"}
+local uid = game.Players.LocalPlayer.userId
+local plist = {"1299452235", "1"}
 if uid == plist then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Afyadh/Kronos-Hub/main/afy.lua'))()
 else

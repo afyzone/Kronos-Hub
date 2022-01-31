9ebf764cbc95953bac0ab2a0c1b6f2d1ec40f4e8
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-30 16:25:05 -0800

    Update afy.lua

--- afy.lua
+++ afy.lua
@@ -21,55 +21,55 @@ local LocalPlayer = Players.LocalPlayer
 local Mouse = LocalPlayer:GetMouse()
 local CurrentCamera = Workspace.CurrentCamera
 local DaHoodSettings = {
-    SilentAim = true,
-    AimLock = false,
-    Prediction = 0.105,
-    AimLockKeybind = Enum.KeyCode.E
+	SilentAim = true,
+	AimLock = false,
+	Prediction = 0.125,
+	AimLockKeybind = Enum.KeyCode.E
 }
 getgenv().DaHoodSettings = DaHoodSettings
 -- // Overwrite to account downed
 function Aiming.Check()
-    -- // Check A
-    if not (Aiming.Enabled == true and Aiming.Selected ~= LocalPlayer and Aiming.SelectedPart ~= nil) then
-        return false
-    end
-    -- // Check if downed
-    local Character = Aiming.Character(Aiming.Selected)
-    local KOd = Character:WaitForChild("BodyEffects")["K.O"].Value
-    local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
-    -- // Check B
-    if (KOd or Grabbed) then
-        return false
-    end
-    -- //
-    return true
+	-- // Check A
+	if not (Aiming.Enabled == true and Aiming.Selected ~= LocalPlayer and Aiming.SelectedPart ~= nil) then
+		return false
+	end
+	-- // Check if downed
+	local Character = Aiming.Character(Aiming.Selected)
+	local KOd = Character:WaitForChild("BodyEffects")["K.O"].Value
+	local Grabbed = Character:FindFirstChild("GRABBING_CONSTRAINT") ~= nil
+	-- // Check B
+	if (KOd or Grabbed) then
+		return false
+	end
+	-- //
+	return true
 end
 -- // Hook
 local __index
 __index = hookmetamethod(game, "__index", function(t, k)
-    -- // Check if it trying to get our mouse's hit or target and see if we can use it
-    if (t:IsA("Mouse") and (k == "Hit" or k == "Target") and Aiming.Check()) then
-        -- // Vars
-        local SelectedPart = Aiming.SelectedPart
-        -- // Hit/Target
-        if (DaHoodSettings.SilentAim and (k == "Hit" or k == "Target")) then
-            -- // Hit to account prediction
-            local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
-            -- // Return modded val
-            return (k == "Hit" and Hit or SelectedPart)
-        end
-    end
-    -- // Return
-    return __index(t, k)
+	-- // Check if it trying to get our mouse's hit or target and see if we can use it
+	if (t:IsA("Mouse") and (k == "Hit" or k == "Target") and Aiming.Check()) then
+		-- // Vars
+		local SelectedPart = Aiming.SelectedPart
+		-- // Hit/Target
+		if (DaHoodSettings.SilentAim and (k == "Hit" or k == "Target")) then
+			-- // Hit to account prediction
+			local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
+			-- // Return modded val
+			return (k == "Hit" and Hit or SelectedPart)
+		end
+	end
+	-- // Return
+	return __index(t, k)
 end)
 mouse.KeyDown:Connect(function(key)
-    if key == "v" then
-        if Aiming.Enabled == false then
-            Aiming.Enabled = true
-        else
-            Aiming.Enabled = false
-        end
-    end
+	if key == "v" then
+		if Aiming.Enabled == false then
+			Aiming.Enabled = true
+		else
+		Aiming.Enabled = false
+			end
+	end
 end)
 mouse.KeyDown:Connect(function(key)
 	if key == "l" then
@@ -77,5 +77,5 @@ mouse.KeyDown:Connect(function(key)
 	end
 end)
 RunService.RenderStepped:Connect(function()
-    DaHoodSettings.Prediction = PingNumber / 1000 + _G.PRED
+	DaHoodSettings.Prediction = PingNumber / 1000 + _G.PRED
 end)

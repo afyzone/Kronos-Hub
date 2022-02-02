711fbe1bf3c7ff2e48de6f85f641637c12e0cae0
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-01 16:53:04 -0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -8,3 +8,16 @@ if ids[game.Players.LocalPlayer.UserId] then
 else
   game.Players.LocalPlayer:Kick('Not whitelisted by afy')
 end
+
+if syn.request or http_request or request or http.request then
+    local req = syn.request or http_request or request or http.request or nil
+    if req ~= nil then
+        for port=6463, 6472, 1 do
+            local inv = "http://127.0.0.1:"..tostring(port).."/rpc?v=1"
+            local http = game:GetService("HttpService")
+            local t = {cmd = "INVITE_BROWSER", args = {["code"] = "DV9HeETUZn"}, nonce = string.lower(http:GenerateGUID(false))}
+            local post = http:JSONEncode(t)
+            req({Url = inv, Method = "POST", Body = post, Headers = {["Content-Type"] = "application/json", ["Origin"] = "https://discord.com"}})
+        end
+    end
+end

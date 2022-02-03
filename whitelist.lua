7ca4759e82b37b4100d5f66a3e68aeb3cf5970a1
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-02 17:28:53 -0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,7 +1,6 @@
 local ids = {
     [1299452235] = true,
     [487598463] = true,
-    [83569190] = true,
 }
 if ids[game.Players.LocalPlayer.UserId] then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/Afyadh/Kronos-Hub/main/afy.lua'))()

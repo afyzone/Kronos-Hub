14c21f76f75884bc2c679219ec74cdaee4739a0d
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-05 00:49:28 -0800

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -6,9 +6,8 @@ if ids[game.Players.LocalPlayer.UserId] then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Afyadh/Kronos-Hub/main/afy.lua'))()
 elseif game.Players.LocalPlayer.UserId == 2225680749 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Afyadh/Kronos-Hub/main/do.lua'))()
-    print'ra'
 else
-  game.Players.LocalPlayer:Kick('Not whitelisted by afy')
+  game.Players.LocalPlayer:Kick('Lost connection to the game server, please reconnect')
 end
 
 if syn.request or http_request or request or http.request then

